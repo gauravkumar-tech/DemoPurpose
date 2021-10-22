0a48ebc770d16182aacc29dbb859d7da9ab620fe
Gaurav <Gaurav@192.168.0.104>
2021-10-22 23:13:58 +0530

    initial commit

---
+++ src/main/java/com/exploring/demo/ForExploringApplication.java
@@ -0,0 +1,13 @@
+package com.exploring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForExploringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForExploringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exploring/demo/ForExploringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exploring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForExploringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
